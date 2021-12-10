31b39d95486854899a17d13f64863a75c4be0dca
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-10 12:15:43 +0900

    ステップ9

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h1>カレンダー</h1>
+    <h1>カレンダー[ {{ type }} ]</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      required: true,
+      validator: (value: string) => {
+        return ['month', 'week', 'day'].includes(value);
+      },
+    },
+  },
+});
 </script>
