f50a08dad15a256ce92cdbb4373a02fe50741f24
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-22 22:12:47 +0900

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
 
@@ -13,4 +13,4 @@ new Vue({
   store,
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
