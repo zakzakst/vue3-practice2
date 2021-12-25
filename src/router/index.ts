f45bd75c55d94347437ac4a7cff82aee68f07709
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-25 19:14:33 +0900

    STEP9-2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import HomeComponent from '@/views/Home.vue';
+import CalendarComponent from '@/views/Calendar.vue';
+import ProfileComponent from '@/views/Profile.vue';
+import ShareComponent from '@/views/Share.vue';
+import SignInComponent from '@/views/SignIn.vue';
 
 Vue.use(VueRouter);
 
