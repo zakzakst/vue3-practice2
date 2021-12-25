cd65f4d98979277849aae4900d292447794a5768
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-25 18:58:17 +0900

    STEP8-3

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,8 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default defineComponent({});
+</script>
