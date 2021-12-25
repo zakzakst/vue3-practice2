9a85f95bb6a8dc5e4f6d60c3bfe43a1fccbd0627
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-25 19:36:20 +0900

    STEP9-3,4,5

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,8 +11,44 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: HomeComponent,
+    meta: {
+      title: 'home',
+    },
+  },
+  {
+    path: '/calendar/:type',
+    name: 'calendar',
+    component: CalendarComponent,
+    props: true,
+    meta: {
+      title: 'calendar',
+    },
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileComponent,
+    meta: {
+      title: 'profile',
+    },
+  },
+  {
+    path: '/share',
+    name: 'share',
+    component: ShareComponent,
+    meta: {
+      title: 'share',
+    },
+  },
+  {
+    path: '/sign-in',
+    name: 'sign-in',
+    component: SignInComponent,
+    meta: {
+      title: 'sign-in',
+    },
   },
 ];
 
@@ -20,6 +56,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.afterEach((to) => {
+  if (!to.meta?.title) {
+    return;
+  }
+  document.title = to.meta.title;
 });
 
 export default router;
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <h1>カレンダー</h1>
+    <h1>カレンダー[{{ type }}]</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Prop({
+    required: true,
+    validator: (value: string) => {
+      return ['month', 'week', 'day'].includes(value);
+    },
+  })
+  private type!: string;
+}
 </script>
