fd218cab27392c4fc6f94fcc7c8a11460064f371
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-31 01:48:09 +0900

    STEP16-3-4

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,19 @@ import '@/plugins/axios';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import '@/plugins/composition-api';
+import * as directives from '@/directives';
+import * as components from '@/components';
 
 Vue.config.productionTip = false;
 
 // import { myExample } from '@/directives/my-example';
 // Vue.directive('myExample', myExample);
 
-import * as directives from '@/directives';
 for (const [key, directive] of Object.entries(directives)) {
   Vue.directive(key, directive);
+}
+for (const [key, component] of Object.entries(components)) {
+  Vue.component(key, component);
 }
 
 new Vue({
